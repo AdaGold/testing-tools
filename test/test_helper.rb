da30930a7033cc7bad29ad9872f8dc768080f2df
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-09-03 09:45:05 -0700

    Update test_helper to exclude test/ from coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter 'test/' # Tests should not be counted toward coverage.
+end
 
 require 'minitest/pride'
 require 'minitest/autorun'
